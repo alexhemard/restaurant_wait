217ce5471e485d2447c2d326ebef42cf2f79bcca
Joel Wietelmann <jwietelmann@gmail.com>
2013-01-12 15:20:54 -0600

    fixing session ID stuff

--- sockets.js
+++ sockets.js
@@ -5,20 +5,14 @@ var models = require('./models')
 module.exports = function(io) {
 
   io.sockets.on('connection', function(socket) {
-
     socket.on('waitTime', function(data) {
-      if (data.headers.cookie) {
-        // if there is, parse the cookie
-        data.cookie = parseCookie(data.headers.cookie);
 
-        console.log(data.cookie['connect.sid']);
-        Restaurant.findById(data.restaurant, function(err, restaurant) {
+      Restaurant.findById(data.restaurant, function(err, restaurant) {
 
-          restaurant.declareWaitTime(data.option, data.cookie['connect.sid']);
-          restaurant.save(); // We don't need to wait for the save to succeed
-          socket.emit('restaurant', restaurant); // Send the updated restaurant
-        });
-      }
+        restaurant.declareWaitTime(data.option, socket.handshake.sessionID);
+        restaurant.save(); // We don't need to wait for the save to succeed
+        socket.emit('restaurant', restaurant); // Send the updated restaurant
+      });
     });
   });
 }
