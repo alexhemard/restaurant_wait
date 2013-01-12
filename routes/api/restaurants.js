b097c261235b8104e267ad5e6f56158ef52ba98c
Alex Hemard <alexhemard@gmail.com>
2013-01-12 02:12:31 -0600

    restaurant route changes

--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -1,6 +1,20 @@
+var models = require('../../models')
+, Restaurant = models.Restaurant;
+
+exports.index = function(req, res, next) {
+  //TODO return closest restaurants
+  Restaurant.find().limit(20).lean().exec(function(err,restaurants) {
+    res.jsonData = restaurants;
+    next();
+  });
+};
+
 exports.show = function(req, res, next) {
-  res.jsonData = {};
-  next();
+  Restaurant.findById(id, function (err, restaurant) {
+    if (err) return res.send(500);
+    return res.jsonData = restaurant;
+    next();
+  });
 };
 
 exports.update = function(req, res, next) {
