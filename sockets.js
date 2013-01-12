f34a17cd444ad274c09260720897276762db5843
Alex Hemard <alexhemard@gmail.com>
2013-01-12 14:28:44 -0600

    socket cookie support?

--- sockets.js
+++ sockets.js
@@ -7,16 +7,18 @@ module.exports = function(io) {
   io.sockets.on('connection', function(socket) {
 
     socket.on('waitTime', function(data) {
+      if (data.headers.cookie) {
+        // if there is, parse the cookie
+        data.cookie = parseCookie(data.headers.cookie);
 
-      Restaurant.findById(data.id, function(err, restaurant) {
+        console.log(data.cookie['connect.sid']);
+        Restaurant.findById(data.restaurant, function(err, restaurant) {
 
-        restaurant.declareWaitTime(data.option); // TODO: define this method
-        restaurant.save(); // We don't need to wait for the save to succeed
-        socket.emit('restaurant', restaurant); // Send the updated restaurant
-      });
-
+          restaurant.declareWaitTime(data.option, data.cookie['connect.sid']);
+          restaurant.save(); // We don't need to wait for the save to succeed
+          socket.emit('restaurant', restaurant); // Send the updated restaurant
+        });
+      }
     });
-
   });
-
-};
+}
