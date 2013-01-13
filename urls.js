d686d785052431ccc1453c6fb621a046cf54e6e9
Joel Wietelmann <jwietelmann@gmail.com>
2013-01-13 13:40:38 -0600

    twilio api interface for reza

--- urls.js
+++ urls.js
@@ -20,6 +20,7 @@ module.exports = function(app){
   app.get('/api/restaurants', routes.api.restaurants.index);
   app.post('/api/restaurants', routes.api.restaurants.create);
   app.get('/api/restaurants/:id', routes.api.restaurants.show);
+  app.post('/api/twilio', routes.api.twilio.acceptData);
 
   // this catch-all route will send JSON for every API route that falls through to this point in the chain
   // WARNING: Sometimes they don't fall through to this point in the chain! Example:
