57bf1bfb582e32da9cb9f8a5cf4a526b18cc1bba
Alex Hemard <alexhemard@gmail.com>
2013-01-13 14:48:37 -0600

    seed vote data

--- restaurantImport.js
+++ restaurantImport.js
@@ -7,6 +7,7 @@ var fs = require('fs')
   , labels
   , recordObject
   , restaurant
+  , _ = require('underscore')
 ;
 
 mongoose.connect(config.mongodb);
@@ -25,7 +26,7 @@ csv().from.stream(fs.createReadStream(__dirname + '/restaurantImport.csv'))
       for(var i = 0; i < data.length; i++) recordObject[labels[i]] = data[i];
       restaurant = new Restaurant();
       restaurant.tourismBoard = recordObject;
-      restaurant.declareWaitTime(Math.floor(Math.random() * 4) + 1, 'swag');
+      _(Math.floor(Math.random() * 6) + 1).times(function(n) { restaurant.declareWaitTime(Math.floor(Math.random() * 4) + 1, 'swag'+n)});
       restaurant.markModified('tourismBoard');
       restaurant.save(function(err) {
         if(err) throw err;
