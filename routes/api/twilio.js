fa5b1187a06de21fbebe9c2fd93eb5f0cdad6be6
Joel Wietelmann <jwietelmann@gmail.com>
2013-01-13 15:00:02 -0600

    twilllllio dammit

--- routes/api/twilio.js
+++ routes/api/twilio.js
@@ -4,11 +4,14 @@ var models = require('../../models')
 
 exports.acceptData = function(req, res, next) {
   var data = req.body.twilio;
-  Restaurant.findById(data.id, function(err, restaurant) {
-    if(err) res.send(404);
-    restaurant.declareWaitTime(data.avail, 'twilio');
-    restaurant.save(); // We don't need to wait for the save to succeed
-    io.sockets.emit('restaurant', restaurant);
+  Restaurant.findById(data.id, function(err, r) {
+    if(err) {
+      console.log(err);
+      return res.send(404);
+    }
+    r.declareWaitTime(data.avail, 'twilio');
+    r.save(); // We don't need to wait for the save to succeed
+    io.sockets.emit('restaurant', r);
     res.send(200);
   });
 };
