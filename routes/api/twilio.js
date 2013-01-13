4e1fd3f03a469f1c5422a04bac70221461544178
Joel Wietelmann <jwietelmann@gmail.com>
2013-01-13 14:52:04 -0600

    twilio support

--- routes/api/twilio.js
+++ routes/api/twilio.js
@@ -1,6 +1,13 @@
+var models = require('../../models')
+  , Restaurant = models.Restaurant
+;
 
 exports.acceptData = function(req, res, next) {
-  console.log('received twilio data');
-  console.log(req.body);
-  res.send(200);
+  var data = req.body.twilio;
+  Restaurant.findById(data.id, function(err, restaurant) {
+    if(err) res.send(404);
+    restaurant.declareWaitTime(data.avail, 'twilio');
+    restaurant.save(); // We don't need to wait for the save to succeed
+    res.send(200);
+  });
 };
