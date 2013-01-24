b2a28d132028d3f803b3c74833a20ea07f391189
Alex Hemard <alex@Alexs-iMac.local>
2013-01-23 20:43:59 -0600

    jazzing up gps

--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -2,25 +2,17 @@ var models = require('../../models')
 , Restaurant = models.Restaurant;
 
 exports.index = function(req, res, next) {
-  var location = null;
-  console.log(req.query);
-  if(req.query.location) {
-    location = req.query.location.split(',').map(function(x) { return parseFloat(x, 10); });
-    //TODO dry this up
-    console.log(location);
-    console.log(Restaurant.schema.methods);
-    Restaurant.findNear(location).limit(40).lean().exec(function(err,restaurants) {
-      res.jsonData = restaurants;
-      next();
-    });
+  var callback = function(err,restaurants) {
+    res.jsonData = restaurants;
+    next();
   }
-  else {
-    Restaurant.find().sort('_id').limit(40).lean().exec(function(err,restaurants) {
-      res.jsonData = restaurants;
-      next();
-    });
+  if(req.body && req.body.location) {
+    var location = req.body.location.split(',').map(function(x) { return parseFloat(x, 10); });  
+    Restaurant.findNear(location).limit(40).lean().exec(callback); 
+  } else {
+    Restaurant.find().sort('_id').limit(40).lean().exec(callback);
   }
-};
+}
 
 exports.show = function(req, res, next) {
   // TODO: Do something with coords
@@ -36,8 +28,6 @@ exports.create = function(req, res, next) {
   restaurant.name = req.body.name;
   restaurant.url = req.body.url;
   restaurant.address = req.body.address;
-  restaurant.location.lat = req.body.lat;
-  restaurant.location.lon = req.body.lon;
   //TODO finish this
 
   restaurant.save(function(err, restaurant) {
