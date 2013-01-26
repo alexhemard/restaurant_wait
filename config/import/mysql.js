71e633763e64b34ad1333d86dd7093eb8790cd4c
Joe Ellis <joe@joeellis.la>
2013-01-25 21:08:40 -0600

    fix restaurantImport script to actually connect to mongoose

--- config/import/mysql.js
+++ config/import/mysql.js
@@ -54,7 +54,6 @@ module.exports = function() {
             details[key] = value;
           }
         });
-
         createRestaurant(details, cuisines);
       });
 
@@ -65,6 +64,7 @@ module.exports = function() {
 
   function createRestaurant(details, cuisines) {
     var restaurant = Restaurant.findOne({ 'tourismBoard.placeID': details.placeID }, function (err, restaurant) {
+      console.log(err);
       if (restaurant == null) {
         restaurant = new Restaurant();
         console.log("Creating record for " + details.name1);
