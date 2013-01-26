58d54ee04c28a8867c94318d9ad7f99b62c8e22c
Joe Ellis <joe@joeellis.la>
2013-01-25 20:59:10 -0600

    made restaurantImport.js just run mysql import

--- restaurantImport.js
+++ restaurantImport.js
@@ -1,45 +1,49 @@
-var fs = require('fs')
-  , csv = require('csv')
-  , config = require('./config')
-  , mongoose = require('mongoose')
-  , models = require('./models')
-  , Restaurant = models.Restaurant
-  , labels
-  , recordObject
-  , restaurant
-  , _ = require('underscore')
-;
+var mysqlImport = require('./config/import/mysql');
 
-mongoose.connect(config.mongodb);
+mysqlImport();
 
-Restaurant.find({}).remove();
+// var fs = require('fs')
+//   , csv = require('csv')
+//   , config = require('./config')
+//   , mongoose = require('mongoose')
+//   , models = require('./models')
+//   , Restaurant = models.Restaurant
+//   , labels
+//   , recordObject
+//   , restaurant
+//   , _ = require('underscore')
+// ;
 
-csv().from.stream(fs.createReadStream(__dirname + '/restaurantImport.csv'))
-  .on('record', function(data, index) {
+// mongoose.connect(config.mongodb);
 
-    if(index == 0) {
-      labels = data;
-      Restaurant.remove();
-    }
-    else {
-      recordObject = {};
-      for(var i = 0; i < data.length; i++) recordObject[labels[i]] = data[i];
-      restaurant = new Restaurant();
-      restaurant.tourismBoard = recordObject;
-      restaurant.location = [recordObject['latitude'],recordObject['longitude']];
-      console.log(restaurant.location);
-      _(Math.floor(Math.random() * 6) + 1).times(function(n) { restaurant.declareWaitTime(Math.floor(Math.random() * 4) + 1, 'swag'+n)});
-      restaurant.markModified('tourismBoard');
-      restaurant.save(function(err) {
-        if(err) throw err;
-      });
-    }
-  })
-  .on('end', function(count){
-    console.log('Number of lines: '+ count);
-  })
-  .on('error', function(error){
-    console.log(error.message);
-  })
-;
+// Restaurant.find({}).remove();
+
+// csv().from.stream(fs.createReadStream(__dirname + '/restaurantImport.csv'))
+//   .on('record', function(data, index) {
+
+//     if(index == 0) {
+//       labels = data;
+//       Restaurant.remove();
+//     }
+//     else {
+//       recordObject = {};
+//       for(var i = 0; i < data.length; i++) recordObject[labels[i]] = data[i];
+//       restaurant = new Restaurant();
+//       restaurant.tourismBoard = recordObject;
+//       restaurant.location = [recordObject['latitude'],recordObject['longitude']];
+//       console.log(restaurant.location);
+//       _(Math.floor(Math.random() * 6) + 1).times(function(n) { restaurant.declareWaitTime(Math.floor(Math.random() * 4) + 1, 'swag'+n)});
+//       restaurant.markModified('tourismBoard');
+//       restaurant.save(function(err) {
+//         if(err) throw err;
+//       });
+//     }
+//   })
+//   .on('end', function(count){
+//     console.log('Number of lines: '+ count);
+//   })
+//   .on('error', function(error){
+//     console.log(error.message);
+//   })
+// ;
 
