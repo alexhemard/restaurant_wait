73ff7e8f4d55d95286ff400d21a68e6f3adc161e
Joe Ellis <joe@joeellis.la>
2013-01-27 21:23:20 -0600

    change redis / mongo endpoints

--- config/production.js
+++ config/production.js
@@ -1,14 +1,15 @@
 exports.loggerFormat = 'dev';
 exports.useErrorHandler = true;
-exports.mongodb = 'mongodb://nodejitsu_jwietelmann:1j1gaj9jah995h8f1ibuf29r9n@ds043937.mongolab.com:43937/nodejitsu_jwietelmann_nodejitsudb3169538436';
+// exports.mongodb = 'mongodb://nodejitsu_jwietelmann:1j1gaj9jah995h8f1ibuf29r9n@ds043937.mongolab.com:43937/nodejitsu_jwietelmann_nodejitsudb3169538436';
+exports.mongodb = 'mongodb://notmc:@ds035147.mongolab.com:35147/eatnownola';
 // mongo --host ds043937.mongolab.com --port 43937 -u nodejitsu_jwietelmann -p 1j1gaj9jah995h8f1ibuf29r9n nodejitsu_jwietelmann_nodejitsudb3169538436
 //exports.mongodb = 'mongodb://swagbasket:swagbasket@ds047307.mongolab.com:47307/restaurantwait';
 exports.sessionSecret = 'sw4gb4$k3t!!!';
 
 exports.socketsRedis = {
-  port: 9799,
-  url: 'slimehead.redistogo.com',
-  password: '283803f32cc90c73b1270f74e55b3666'
+  port: 9242,
+  url: 'dory.redistogo.com',
+  password: '181c9f6aaa8aef23b0b51ba9df2bdfc0'
 };
 
 // This is NOTMC's real account
